b714e7c1d3dcf51682602aaf8855b7bc4503dae5
Micah Williamson <micahwllmsn@gmail.com>
2016-06-07 22:44:35 -0500

    new features werent included in index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import {_for} from './for';
+import {_forLazy} from './forLazy';
 import {_forEach} from './forEach';
+import {_forEachLazy} from './forEachLazy';
 import {_while} from './while';
 
 export = {
   for:      _for,
+  forLazy: _forLazy,
   forEach:  _forEach,
+  forEachLazy: _forEachLazy,
   while:    _while
 }
